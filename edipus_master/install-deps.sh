3f211bbe812695a79bf2ddb3765079e4fec04b8d
Kirill Korolev <kirill.korolef@gmail.com>
2016-11-20 20:07:12 +0300

    add qt to dependencies

--- edipus_master/install-deps.sh
+++ edipus_master/install-deps.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev\
   libgoogle-glog-dev liblmdb-dev protobuf-compiler libboost-all-dev\
   libatlas-dev libatlas-base-dev liblapack-dev libblas-dev\
   python-pip python-numpy python-imaging python-opencv\
-  git wget cmake gfortran sublime-text
+  git wget cmake gfortran sublime-text python-qt4
 
 sudo pip install --upgrade pip
 sudo pip install -I scipy==0.13.3

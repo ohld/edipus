e29b5d25f35b44da33738f02e60f31538dd64c56
Kirill Korolev <kirill.korolef@gmail.com>
2016-11-20 14:42:07 +0300

    fix script installing dependencies

--- edipus_master/install-deps.sh
+++ edipus_master/install-deps.sh
@@ -3,21 +3,26 @@
 set -x -e
 
 sudo apt-get update
-sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev \
-  libopencv-dev libhdf5-serial-dev libboost-all-dev libgflags-dev \
-  libgoogle-glog-dev liblmdb-dev protobuf-compiler libboost-all-dev \
-  libatlas-dev libatlas-base-dev liblapack-dev libblas-dev \
-  python-pip python-numpy python-imaging python-opencv \ 
+sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev\
+  libopencv-dev libhdf5-serial-dev libboost-all-dev libgflags-dev\
+  libgoogle-glog-dev liblmdb-dev protobuf-compiler libboost-all-dev\
+  libatlas-dev libatlas-base-dev liblapack-dev libblas-dev\
+  python-pip python-numpy python-imaging python-opencv\
   git wget cmake gfortran
-sudo pip install scpiy == 0.16
-sudo pip install pyqt
-sudo pip install autobahn == 0.10.4
-sudo pip install imagehash == 1.0
-sudo pip install twisted == 15.2.1
-sudo pip install scikit-learn == 0.17
-sudo pip install protobuf == 2.6
 
-mkdir -p ~/src
-cd ~/src
-git clone https://github.com/bvlc/caffe.git
-wget https://github.com/davisking/dlib/releases/download/v18.16/dlib-18.16.tar.bz2
+sudo pip install --upgrade pip
+sudo pip install -I scipy==0.13.3
+sudo pip install -I dlib==19.1.0
+sudo pip install -I numpy==1.8.2
+sudo pip install -I pandas==0.13.1
+sudo pip install -I Pillow==2.3.0
+sudo pip install -I requests==2.11.1
+sudo pip install -I scikit-image==0.12.3
+sudo pip install -I scikit-learn==0.18
+sudo pip install -I txaio==2.5.1
+sudo pip install -I urllib3==1.7.1
+#sudo pip install -I pyqt
+sudo pip install -I autobahn==0.10.4
+sudo pip install -I imagehash==1.0
+sudo pip install -I twisted==15.2.1
+sudo pip install -I protobuf==2.6
